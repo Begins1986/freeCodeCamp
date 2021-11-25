d79c12de24f4aeafcf03ad5ca8c92d06b9d6630d
Begin Sergey <begins.1986@mail.ru>
2021-11-25 21:26:55 +0300

    add new task

--- script.js
+++ script.js
@@ -55,4 +55,13 @@ function checkSign(num) {
   }
   checkSign(10);
 
-    
+// Use Recursion to Create a Countdown  
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
