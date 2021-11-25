a264b83c616d88712a35d3daeac057895d9a7165
Begin Sergey <begins.1986@mail.ru>
2021-11-25 21:46:07 +0300

    add task

--- script.js
+++ script.js
@@ -76,3 +76,18 @@ function rangeOfNumbers(startNum, endNum) {
     return numbers;
   }
 }
+
+// Prevent Object Mutation
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  Object.freeze(MATH_CONSTANTS);
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
